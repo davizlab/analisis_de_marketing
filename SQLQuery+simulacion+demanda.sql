f79f5b5c5242084e86dd4a8ec0f2aec8d8376ae6
Daviz Lab <159964033+davizlab@users.noreply.github.com>
2025-11-28 19:40:18 -0800

    Delete SQLQuery+simulacion+demanda.sql

--- SQLQuery+simulacion+demanda.sql
+++
@@ -1,9 +0,0 @@
-select
-	t1.ProductID,
-	t2.Name as 'producto',
-	sum(t1.OrderQty) as 'demanda',
-	avg(t1.UnitPrice) as 'precio_promedio',
-	avg(t2.StandardCost) as 'costo'
-from Sales.SalesOrderDetail as t1
-	inner join Production.Product as t2 on t1.ProductID = t2.ProductID
-	group by t1.ProductID, t2.Name
